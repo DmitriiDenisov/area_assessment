ab9070dda7f2b34c0366866f77851e00307f3404
Mangutov, Oleg <omangutov@deloitte.ru>
2017-03-28 15:05:40 +0300

    Make geotiff_to_shp_file to write each polygon as separate feature on layer

--- scripts/geo/geotiff_to_shp_file.py
+++ scripts/geo/geotiff_to_shp_file.py
@@ -4,6 +4,7 @@ import glob
 import sys
 import cv2
 import gdal
+from math import floor
 from gdal import ogr, osr
 from gdalconst import GA_ReadOnly
 from shapely.geometry import Polygon, MultiPolygon
@@ -19,10 +20,16 @@ parser.add_argument('-v', '--verbose', dest='v', action='store_const',
                     const=True, default=False,
                     help='print the processing details')
 parser.add_argument('-l', '--layer_name', dest='ln', type=str,
-                    default='default',
+                    default='default_layer',
                     help='layer name')
+parser.add_argument('-f', '--feature_name', dest='fn', type=str,
+                    default='feature',
+                    help='feature name')
 parser.add_argument('--target_EPSG', dest='t_epsg', type=int,
                     help='EPSG code to reproject geometry')
+parser.add_argument('--mask_threshold', dest='m_thre', type=float,
+                    default=0.5,
+                    help='threshold to for separation of polygons from background')
 
 try:
     args = parser.parse_args()
@@ -44,8 +51,8 @@ for f in [os.path.basename(f) for f in glob.glob(src_dir) if os.path.isfile(os.p
     mask = cv2.imread(cur_file_path, cv2.IMREAD_GRAYSCALE)
     if mask is None:
         continue
-    mask[mask < 127] = 0
-    mask[mask >= 127] = 1
+    mask[mask < floor(0.5 * 255)] = 0
+    mask[mask >= floor(0.5 * 255)] = 1
 
     image_polygons = mask_to_polygons(mask, epsilon=2, min_area=0.02)
 
@@ -74,36 +81,35 @@ source_srs = osr.SpatialReference()
 source_srs.ImportFromWkt(projection_wkt)
 srs = source_srs
 
-
-# Make a geometry, from Shapely object
-geom = ogr.CreateGeometryFromWkb(mult_p.wkb)
-
 if (args.t_epsg is not None):
     target_srs = osr.SpatialReference()
     target_srs.ImportFromEPSG(args.t_epsg)
-
     transform = osr.CoordinateTransformation(source_srs, target_srs)
     srs = target_srs
+else:
+    transform = osr.CoordinateTransformation(source_srs, source_srs)
 
-    geom.Transform(transform)
-
+# create new layer definition and define columns
+# of attribute table
 layer = ds.CreateLayer(args.ln, srs, ogr.wkbMultiPolygon)
+layer.CreateField(ogr.FieldDefn("Name", ogr.OFTString))
+layer.CreateField(ogr.FieldDefn("Area", ogr.OFTReal))
 
-# Add one attribute
-layer.CreateField(ogr.FieldDefn(args.ln, ogr.OFTInteger))
-defn = layer.GetLayerDefn()
-
-# Create a new feature (attribute and geometry)
-feat = ogr.Feature(defn)
-feat.SetField(args.ln, 1)
-
-feat.SetGeometry(geom)
-
-layer.CreateFeature(feat)
-
-feat = geom = None  # destroy these
+for i, p in enumerate(mult_p):
+    # create bew feature
+    feature = ogr.Feature(layer.GetLayerDefn())
+    # create geometry from polygon
+    geom_poly = ogr.CreateGeometryFromWkb(p.wkb)
+    # transform geometry from source_srs to target_srs
+    geom_poly.Transform(transform)
+    # set the feature geometry and attributes
+    feature.SetGeometry(geom_poly)
+    feature.SetField("Area", geom_poly.GetArea())
+    feature.SetField("Name", "{}_{}".format(args.fn, i))
+    # add the feature in the layer
+    layer.CreateFeature(feature)
+    # Dereference the feature
+    feature = None
 
 # Save and close everything
 ds = layer = feat = geom = None
-
-print()
